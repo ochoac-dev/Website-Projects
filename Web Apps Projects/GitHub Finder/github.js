cf2ea8e390d8be4ef1db84b72fddfec4844aadd2
ochoac-dev <77684527+ochoac-dev@users.noreply.github.com>
2021-12-01 19:20:50 -0800

    commit to main

---
+++ Web Apps Projects/GitHub Finder/github.js
@@ -0,0 +1,25 @@
+class GitHub {
+    constructor(){
+        this.client_id = '0bcd08bed9234d270454';
+        this.client_secret = 'fbe1d74db892c5fb357345ad18f81bd3817ec211'
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc'
+
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret} `);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos? per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret} `);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+
+    }
+
+}
